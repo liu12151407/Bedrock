0a366545acb149a148f8edbc26aa27d6fdf16e72
LIU YINGLONG <635536177@qq.com>
2021-02-07 10:09:16 +0800

    更新环境配置

--- lib/base_framework/config/app_config.dart
+++ lib/base_framework/config/app_config.dart
@@ -1,23 +1,7 @@
-
-
-
-
-
-
-
 import 'package:flutter_bedrock/base_framework/config/storage_manager.dart';
-
 class AppConfig{
-
-
   static init()async{
-
     ///启动本地存储工具
     await StorageManager.init();
-
-
-
-
-
   }
 }
